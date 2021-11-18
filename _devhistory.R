7eb505b6866104012bb261e632924d099eb4298e
Cml_Magneville <camille.magneville@gmail.com>
2021-11-18 15:19:03 +0100

    :bulb: Update documentation

--- _devhistory.R
+++ _devhistory.R
@@ -3,3 +3,7 @@ usethis::use_build_ignore("_devhistory.R")
 usethis::use_git(message = ":tada: Initial commit")
 
 usethis::edit_file("DESCRIPTION")
+
+usethis::use_git(message = ":bulb: Edit package metadata")
+
+usethis::use_package_doc()
