ef2ab19706e4c28c098fab96592300613cb8d353
Cml_Magneville <camille.magneville@gmail.com>
2022-04-19 16:47:23 +0200

    :tada: script for 3 intermediate fcts

---
+++ R/3_SmaxN_big_interval.R
@@ -0,0 +1,12 @@
+################################################################################
+##
+## 3_SmaxN_big_interval.R
+##
+## This script gathers three functions used to find the path to the biggest 
+## SmaxN in a given bloc (interval of time begining at a given timestep).
+##
+## 19/04/2022
+##
+## Camille Magneville
+##
+################################################################################
